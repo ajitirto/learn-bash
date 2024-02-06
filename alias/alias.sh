9909e93f8abddf1cda6a757779842554c37de24b
ajitirto <ajiwoke@gmail.com>
2024-02-06 21:10:29 +0700

    feat: add alias

--- alias/alias.sh
+++ alias/alias.sh
@@ -1,2 +1,23 @@
 # 1 learn alias / shortcut in bash
+# terminator sue shorcut
 
+
+# change directory
+alias .1="cd .."
+alias .2="cd ../.."
+alias .3="cd ../../.."
+alias .4="cd ../../../.."
+alias .5="cd ../../../../.."
+
+# Estimate file space usage to maximum depth
+alias du1="du -d 1"
+
+# Ping Commands
+alias pg="ping google.com -c 5"
+
+#Install & Update utilties
+alias sai="sudo apt install"
+alias sau="sudo apt update"
+
+# Show open ports
+alias ports='sudo netstat -tulanp'
